6c1b97317c128aafdb33a39e14ae6541a14f9f1f
HitzCommitted <hitzcommitted@gmail.com>
2023-11-04 21:04:17 +0300

    changed background color of app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.limeAccent,
         body: Center(
           child: Text('Welcome Coding With Hitz'),
         ),
