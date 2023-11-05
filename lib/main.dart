83d20811e66c5e87f26a51f2a4bd040822b97fec
HitzCommitted <hitzcommitted@gmail.com>
2023-11-05 09:59:40 +0300

    improved color using gradient

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.limeAccent,
-        body: Center(
-          child: Text('Welcome Coding With Hitz'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 2, 63, 80),
+                Color.fromARGB(255, 2, 80, 90),
+              ],
+              begin: Alignment.topRight,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: const Center(
+            child: Text('Welcome Coding With Hitz'),
+          ),
         ),
       ),
     ),
